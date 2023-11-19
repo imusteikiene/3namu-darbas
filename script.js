863e893487caf838b8275a02bf5dbf2f2fc9385b
imusteikiene <149151782+imusteikiene@users.noreply.github.com>
2023-11-19 13:37:43 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+// 3. Ciklų užduotis:
+
+// 1. Padaryk, kad kompiuteris 10 kartų paconsole'intų tavo vardą
+
+// 2. Pakoreguok pirmą pratimą, kad tiek vardas, tiek kiek kartų kartos - būtų kintamieji.
+
+// 3. Prie savo vardo atspaudink ir 'i' raidę, t.y. kelintas ciklas yra (pvz.: "0. Petras", "1. Petras", "2. Petras")...
+
+// Parašyk for loopą, kuris atspaudins nuo 10 iki 1 countdown konsolėje.
+
+
+let vardas = "Iveta";
+let kartai = 10;
+
+for (let i = 0; i < 10; i++){
+    console.log("Iveta");
+}
+
+for (let i = 10; i >= 1; i--) {
+    console.log(i);
+  }
